13d83dc6215963cdaa84d17afcb4be2cb0169ab4
İlayda Şenocak <isenocak229@gmail.com>
2024-12-24 16:09:30 +0300

    İlk commit

---
+++ TopluMailGonderme/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace TopluMailGonderme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxEmails = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonTxt = new System.Windows.Forms.Button();
+            this.textBoxHTMLBody = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSMTP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPORT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxPASSWORD = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxUSER = new System.Windows.Forms.TextBox();
+            this.checkBoxSSL = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBoxLoading = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLoading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxEmails
+            // 
+            this.listBoxEmails.FormattingEnabled = true;
+            this.listBoxEmails.ItemHeight = 16;
+            this.listBoxEmails.Location = new System.Drawing.Point(30, 106);
+            this.listBoxEmails.Name = "listBoxEmails";
+            this.listBoxEmails.Size = new System.Drawing.Size(237, 372);
+            this.listBoxEmails.TabIndex = 0;
+            this.listBoxEmails.SelectedIndexChanged += new System.EventHandler(this.listBoxEmails_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Emails";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(178, 67);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonTxt
+            // 
+            this.buttonTxt.Location = new System.Drawing.Point(97, 67);
+            this.buttonTxt.Name = "buttonTxt";
+            this.buttonTxt.Size = new System.Drawing.Size(75, 23);
+            this.buttonTxt.TabIndex = 3;
+            this.buttonTxt.Text = "Text file";
+            this.buttonTxt.UseVisualStyleBackColor = true;
+            this.buttonTxt.Click += new System.EventHandler(this.buttonTxt_Click);
+            // 
+            // textBoxHTMLBody
+            // 
+            this.textBoxHTMLBody.Location = new System.Drawing.Point(306, 106);
+            this.textBoxHTMLBody.Multiline = true;
+            this.textBoxHTMLBody.Name = "textBoxHTMLBody";
+            this.textBoxHTMLBody.Size = new System.Drawing.Size(302, 372);
+            this.textBoxHTMLBody.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email body";
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(378, 45);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(230, 22);
+            this.textBoxSubject.TabIndex = 6;
+            this.textBoxSubject.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(303, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subject";
+            // 
+            // textBoxSMTP
+            // 
+            this.textBoxSMTP.Location = new System.Drawing.Point(762, 106);
+            this.textBoxSMTP.Name = "textBoxSMTP";
+            this.textBoxSMTP.Size = new System.Drawing.Size(230, 22);
+            this.textBoxSMTP.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(649, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Server IP/Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(649, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Port:";
+            // 
+            // textBoxPORT
+            // 
+            this.textBoxPORT.Location = new System.Drawing.Point(762, 155);
+            this.textBoxPORT.Name = "textBoxPORT";
+            this.textBoxPORT.Size = new System.Drawing.Size(230, 22);
+            this.textBoxPORT.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(649, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Password:";
+            // 
+            // textBoxPASSWORD
+            // 
+            this.textBoxPASSWORD.Location = new System.Drawing.Point(762, 251);
+            this.textBoxPASSWORD.Name = "textBoxPASSWORD";
+            this.textBoxPASSWORD.Size = new System.Drawing.Size(230, 22);
+            this.textBoxPASSWORD.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(649, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "User:";
+            // 
+            // textBoxUSER
+            // 
+            this.textBoxUSER.Location = new System.Drawing.Point(762, 202);
+            this.textBoxUSER.Name = "textBoxUSER";
+            this.textBoxUSER.Size = new System.Drawing.Size(230, 22);
+            this.textBoxUSER.TabIndex = 12;
+            // 
+            // checkBoxSSL
+            // 
+            this.checkBoxSSL.AutoSize = true;
+            this.checkBoxSSL.Location = new System.Drawing.Point(1010, 111);
+            this.checkBoxSSL.Name = "checkBoxSSL";
+            this.checkBoxSSL.Size = new System.Drawing.Size(56, 21);
+            this.checkBoxSSL.TabIndex = 16;
+            this.checkBoxSSL.Text = "SSL";
+            this.checkBoxSSL.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(762, 300);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(190, 21);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "Authentication Required?";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // pictureBoxLoading
+            // 
+            this.pictureBoxLoading.Image = global::TopluMailGonderme.Properties.Resources.lg_dual_ring_loader;
+            this.pictureBoxLoading.Location = new System.Drawing.Point(783, 353);
+            this.pictureBoxLoading.Name = "pictureBoxLoading";
+            this.pictureBoxLoading.Size = new System.Drawing.Size(180, 125);
+            this.pictureBoxLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLoading.TabIndex = 18;
+            this.pictureBoxLoading.TabStop = false;
+            this.pictureBoxLoading.Visible = false;
+            this.pictureBoxLoading.Click += new System.EventHandler(this.pictureBoxLoading_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 559);
+            this.Controls.Add(this.pictureBoxLoading);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBoxSSL);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxPASSWORD);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxUSER);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPORT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxSMTP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxHTMLBody);
+            this.Controls.Add(this.buttonTxt);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxEmails);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLoading)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxEmails;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonTxt;
+        private System.Windows.Forms.TextBox textBoxHTMLBody;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSMTP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPORT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxPASSWORD;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxUSER;
+        private System.Windows.Forms.CheckBox checkBoxSSL;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBoxLoading;
+    }
+}
+
---
+++ TopluMailGonderme/Form1.cs
@@ -0,0 +1,101 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopluMailGonderme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxEmails_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Txt dosyasındaki mailleri çekme
+        private void buttonTxt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Multiselect = false;
+
+            DialogResult dr = openFile.ShowDialog();
+            if(dr == DialogResult.OK)
+            {
+                System.IO.StreamReader file=new System.IO.StreamReader(@openFile.FileName);
+                string line;
+                while((line = file.ReadLine()) != null)
+                {
+                    listBoxEmails.Items.Add(line);
+
+                }
+                file.Close();
+            }
+
+        }
+
+        private void pictureBoxLoading_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            buttonSend.Enabled = false;
+            pictureBoxLoading.Visible = true;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            SmtpClient client = new SmtpClient();
+            client.Timeout = 30000;
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+            BodyBuilder builder = new BodyBuilder();
+            builder.HtmlBody = textBoxHTMLBody.Text;
+
+            MimeMessage mail = new MimeMessage();
+            mail.Subject = textBoxSubject.Text;
+            mail.Body = builder.ToMessageBody();
+
+            client.Connect(textBoxSMTP.Text, int.Parse(textBoxPORT.Text), checkBoxSSL.Checked);
+            client.Authenticate(textBoxUSER.Text, textBoxPASSWORD.Text);
+
+            foreach (string email in listBoxEmails.Items)
+            {
+                mail.From.Add(new MailboxAddress("Toplu Mail Otomasyonu", textBoxUSER.Text));
+                mail.Bcc.Add(new MailboxAddress(null,email));
+                client.Send(mail);
+                Thread.Sleep(1000);
+
+            }
+            client.Disconnect(true);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            buttonSend.Enabled = true;
+            pictureBoxLoading.Visible = true;
+            MessageBox.Show("Done");
+
+        }
+    }
+}
